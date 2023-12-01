f44cf32c0331ee2b03c0b5cecd12efdf3a720936
qnfm <uzinag@163.com>
2023-12-01 19:36:41 +0000

    Refactor the source code and the structure of cmake files. Remove uncessary header file Add facebook FindSodium.cmake

--- README.md
+++ README.md
@@ -1,26 +1,64 @@
 # Standalone-OceanKeyak
-an AE scheme extracted from XKCP(eXtended Keccak Code Package)
+An cross-platform file encryption tool with AE scheme extracted from XKCP(eXtended Keccak Code Package)
 * [`Keyak`](https://github.com/XKCP/XKCP/blob/master/doc/Keyak-documentation.h), the authenticated encryption schemes River, Lake, Sea, Ocean and Lunar Keyak.This repo only extracts the OceanKeyak 
+
+# Dependencies
+
+## CMake
+
+Keyak requires CMake 3.10.0 or newer in order to take advantage of improved support. If you're using Ubuntu 18.04 or newer you should already have a suitable version. If you're using an older distribution (e.g. 16.04) you will need to install a compatible version of CMake yourself. Also, Keyak utilizes the libsodium api to generate unpredictable data, suitable for creating secret keys, so you should install [libsodium](https://download.libsodium.org/libsodium/releases/) into your system path if you are using linux distribution, or if you are using windows, you can type the following commad to set the sodium_DIR environment variable to point at the directory of pre-built libsodium from official website.
+
+```cmd
+set sodium_DIR path_to_pre-built_lib
+```
+
+To install libsodium on Linux:
+
+```bash
+sudo apt install libsodium-dev
+```
 
 # Build
 
 Clone the code and change into the source code directory
 
-$ git clone https://github.com/qnfm/Standalone-OceanKeyak && cd Standalone-OceanKeyak
+```bash
+git clone https://github.com/qnfm/Standalone-OceanKeyak && cd Standalone-OceanKeyak
+```
 
-If you have downloaded the ninja_build binary, the build command is:
+If you have downloaded the ninja_build binary on Windows, the build command is:
 
-$ cmake "-DCMAKE_MAKE_PROGRAM=C:/path_to_ninja/ninja.exe" -G Ninja -S . -B cmake-build-debug
+```bash
+cmake "-DCMAKE_MAKE_PROGRAM=C:/path_to_ninja/ninja.exe" -G Ninja -S . -B cmake-build-debug -Dsodium_USE_STATIC_LIBS=ON
+```
 
 Or run build without ninja:
 
-$ cmake -S . -B cmake-build-debug
+```bash
+cmake -S . -B cmake-build-debug -Dsodium_USE_STATIC_LIBS=ON
+```
 
 Final step: link the program:
 
-$ cmake --build cmake-build-debug --target Keyak -j 3
+```bash
+cmake --build cmake-build-debug --target Keyak -j 8
+```
 
-The binary is located in the x64(x84)/Debug directory
+The binary is located in the cmake-build-debug/Release|Debug directory on Windows and cmake-build-debug/ directory on linux
+
+# Usage
+
+To encrypt, you simply need to provide thepath to the filename, remember to keep the generated two files safely and secretly. If you lose these two file (one with .key surfix and the other with .Keyak surfix), the original file will be lost forever !
+
+```
+Keyak file_to_be_encrypted
+```
+
+To decrypt, put the two output files at the same directory and simply call:
+
+```
+Keyak path_to_key.key path_to_ciphertext.Keyak
+```
 
 # Under which license is the XKCP distributed?
 
--- main.c
+++ main.c
@@ -2,35 +2,213 @@
 The eXtended Keccak Code Package (XKCP)
 https://github.com/XKCP/XKCP
 
-Keyak, designed by Guido Bertoni, Joan Daemen, Michaël Peeters, Gilles Van Assche and Ronny Van Keer.
+Keyak, designed by Guido Bertoni, Joan Daemen, Michaël Peeters, Gilles Van
+Assche and Ronny Van Keer.
 
-Implementation by Gilles Van Assche and Ronny Van Keer, hereby denoted as "the implementer".
+Implementation by Gilles Van Assche and Ronny Van Keer, hereby denoted as "the
+implementer".
 
-For more information, feedback or questions, please refer to the Keccak Team website:
-https://keccak.team/
+For more information, feedback or questions, please refer to the Keccak Team
+website: https://keccak.team/
 
 To the extent possible under law, the implementer has waived all copyright
 and related or neighboring rights to the source code in this file.
 http://creativecommons.org/publicdomain/zero/1.0/
 */
 
-#include <string.h>
-#include "test_crypto_aead.h"
+#if defined _WIN32
+#ifndef SODIUM_STATIC
+#define SODIUM_STATIC
+#endif
+#endif
 
-int main()
-{
-    const unsigned char* key = (const unsigned char*)
-        "\x5a\x4b\x3c\x2d\x1e\x0f\x00\xf1\xe2\xd3\xc4\xb5\xa6\x97\x88\x79";
-    const unsigned char* nonce_ = (const unsigned char*)
-        "\x6b\x4c\x2d\x0e\xef\xd0\xb1\x92\x72\x53\x34\x15\xf6\xd7\xb8\x99";
-    const unsigned char* AD = (const unsigned char*)
-        "\x32\xf3\xb4\x75\x35\xf6";
-    const unsigned char* plaintext = (const unsigned char*)
-        "\xe4\x65\xe5\x66\xe6\x67\xe7";
-    const unsigned char* ciphertext = (const unsigned char*)
-        "\xfd\x67\x7c\x8f\x28\xc8\x64\x6f\x2d\xf1\x11\xe5\x95\xcf\x14\xed\x4b\x3b\xbf\xe0\xd6\xc3\xac";
-    unsigned char nonce[150];
-    memset(nonce, 0, 150);
-    memcpy(nonce, nonce_, 16);
-    return test_crypto_aead(key, 16, nonce, 150, AD, 6, plaintext, 7, ciphertext, 16);
+#define ADsize 20
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "encrypt.h"
+#include "sodium.h"
+
+int main(int argc, char **argv) {
+  const size_t keysize = 16, noncesize = 16, tagsize = 16;
+  // domain seperator: "StandaloneOceanKeyak"
+  // const unsigned char* AD = (const unsigned char*)"\x32\xf3\xb4\x75\x35\xf6";
+  const unsigned char AD[ADsize] = {0x53, 0x74, 0x61, 0x6e, 0x64, 0x61, 0x6c,
+                                    0x6f, 0x6e, 0x65, 0x4f, 0x63, 0x65, 0x61,
+                                    0x6e, 0x4b, 0x65, 0x79, 0x61, 0x6b};
+  char ptstr[200];
+  char ctstr[200];
+  char kstr[200];
+  unsigned char nonce[150];
+  memset(ptstr, 0, 200);
+  memset(ctstr, 0, 200);
+  memset(kstr, 0, 200);
+  memset(nonce, 0, 150);
+
+  FILE *output_file, *input_file, *key_file;
+  int err;
+
+  struct stat sbk;
+  struct stat sbpt;
+  struct stat sbct;
+
+  unsigned char *plaintext;
+  unsigned char *ciphertext;
+  unsigned char *key;
+
+  int retcode;
+
+  if (sodium_init() < 0) {
+    perror("Failed to init libsodium\n");
+    exit(EXIT_FAILURE);
+  }
+  // encryption mode args:{plaintext filename}
+  if (argc == 2) {
+    strcpy(ptstr, argv[1]);
+    strcpy(ctstr, argv[1]);
+    strcat(ctstr, ".Keyak");
+    strcpy(kstr, ptstr);
+    strcat(kstr, ".key");
+
+#if defined _WIN32
+    err = fopen_s(&key_file, kstr, "wb");
+    if (err != 0) perror("The key file was not opened");
+
+    err = fopen_s(&input_file, ptstr, "rb");
+    if (err != 0) perror("The input file was not opened");
+
+    err = fopen_s(&output_file, ctstr, "wb");
+    if (err != 0) perror("The output was not opened");
+#else
+    key_file = fopen(kstr, "wb");
+    if (!key_file) perror("The key file was not opened");
+
+    input_file = fopen(ptstr, "rb");
+    if (!input_file) perror("The input file was not opened");
+
+    output_file = fopen(ctstr, "wb");
+    if (!output_file) perror("The output was not opened");
+    if (!key_file || !input_file || !output_file) return -1;
+#endif
+
+    if (stat(kstr, &sbk) == -1 || stat(ptstr, &sbpt) == -1 ||
+        stat(ctstr, &sbct) == -1) {
+      perror("stat");
+      exit(EXIT_FAILURE);
+    }
+    if (sbk.st_size != 0 || sbpt.st_size == 0 || sbct.st_size != 0) {
+      perror("The key/ciphertext is not empty or the plaintext file is empty");
+      exit(EXIT_FAILURE);
+    }
+
+    plaintext = malloc(sbpt.st_size);
+    ciphertext = malloc(sbpt.st_size + tagsize);
+    // keyfile is: key||nonce
+    key = malloc(keysize + noncesize);
+
+    randombytes_buf(key, keysize);
+    fwrite(key, sizeof(unsigned char), keysize + noncesize, key_file);
+
+    memcpy(nonce, key + keysize, 16);
+
+#if defined _WIN32
+    fread_s(plaintext, sbpt.st_size, sbpt.st_size, sizeof(unsigned char),
+            input_file);
+#else
+    fread(plaintext, sizeof(unsigned char), sbpt.st_size, input_file);
+#endif
+
+    unsigned long long clen;
+    retcode = crypto_aead_encrypt(ciphertext, &clen, plaintext, sbpt.st_size,
+                                  AD, ADsize, 0, nonce, key);
+    if (retcode != 0) {
+      perror("!!! crypto_aead_encrypt() did not return 0.\n");
+      exit(EXIT_FAILURE);
+    }
+    fwrite(ciphertext, sizeof(unsigned char), clen, output_file);
+  }
+  // decryption mode args:{keyfile, ciphertext}
+  else if (argc == 3) {
+    strcpy(kstr, argv[1]);
+    strcpy(ctstr, argv[2]);
+    strncpy(ptstr, ctstr, strlen(argv[2]) + 1 - sizeof(".Keyak"));
+
+#if defined _WIN32
+    err = fopen_s(&key_file, kstr, "rb");
+    if (err != 0) perror("Key file was not opened");
+
+    err = fopen_s(&input_file, ctstr, "rb");
+    if (err != 0) perror("Input file was not opened");
+
+    err = fopen_s(&output_file, ptstr, "wb");
+    if (err != 0) perror("Output was not opened");
+#else
+    key_file = fopen(kstr, "rb");
+    if (!key_file) perror("The key file was not opened");
+
+    input_file = fopen(ctstr, "rb");
+    if (!input_file) perror("The input file was not opened");
+
+    output_file = fopen(ptstr, "wb");
+    if (!output_file) perror("The output was not opened");
+    if (!key_file || !input_file || !output_file) return -1;
+#endif
+
+    if (stat(kstr, &sbk) == -1 || stat(ptstr, &sbpt) == -1 ||
+        stat(ctstr, &sbct) == -1) {
+      perror("stat");
+      exit(EXIT_FAILURE);
+    }
+
+    if (sbk.st_size == 0 || sbct.st_size == 0 || sbpt.st_size != 0) {
+      perror("The key/ciphertext is empty or the plaintext file is not empty");
+      exit(EXIT_FAILURE);
+    }
+
+    plaintext = malloc(sbct.st_size - tagsize);
+    ciphertext = malloc(sbct.st_size);
+    // keyfile is: key||nonce
+    key = malloc(keysize + noncesize);
+
+#if defined _WIN32
+    fread_s(key, sbk.st_size, sizeof(unsigned char), sbk.st_size, key_file);
+    fread_s(ciphertext, sbct.st_size, sizeof(unsigned char), sbct.st_size,
+            input_file);
+#else
+    fread(key, sizeof(unsigned char), sbk.st_size, key_file);
+    fread(ciphertext, sizeof(unsigned char), sbct.st_size, input_file);
+#endif
+
+    memcpy(nonce, key + keysize, 16);
+
+    unsigned long long mlen;
+    retcode = crypto_aead_decrypt(plaintext, &mlen, 0, ciphertext, sbct.st_size,
+                                  AD, ADsize, nonce, key);
+    if (retcode != 0) {
+      perror("!!! crypto_aead_decrypt() did not return 0");
+      exit(EXIT_FAILURE);
+    }
+
+    if (mlen != sbct.st_size - tagsize) {
+      perror("!!! plaintext length mistach.");
+      exit(EXIT_FAILURE);
+    }
+    fwrite(plaintext, sizeof(unsigned char), mlen, output_file);
+  }
+  // unexpected behaviour
+  else {
+    perror(
+        "Error, usage: path_to_keyfile path_to_plaintext path_to_ciphertext");
+    exit(EXIT_FAILURE);
+  }
+
+  fclose(input_file);
+  fclose(output_file);
+  fclose(key_file);
+  free(plaintext);
+  free(ciphertext);
+  free(key);
+  return retcode;
 }
